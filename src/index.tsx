c61b3dde3e3cf410d6630c0ace7301bbe8814bf7
Roman Sevryuk <Sevryuk.Roman@outlook.com>
2023-01-02 20:30:40 +0300

    style(App) refactor border-bottom

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals'
 import store from './s2-homeworks/hw10/bll/store'
 import {Provider} from 'react-redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot( document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         {/*для дз 10*/}
