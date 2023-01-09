2c483595a9d353bfbdbc2a32944c93d0056c8357
Roman Sevryuk <Sevryuk.Roman@outlook.com>
2023-01-09 03:29:14 +0300

    commit for deploy homework hw08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,20 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            const sortedByName = [...state]
+            if (action.payload === 'up')
+                sortedByName.sort((a, b) => a.name > b.name ? 1 : -1)
+            if (action.payload === 'down')
+                sortedByName.sort((a, b) => a.name > b.name ? -1 : 1)
             // sort() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return sortedByName // need to fix
         }
         case 'check': {
             // filter() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload) // need to fix
         }
         default:
             return state
