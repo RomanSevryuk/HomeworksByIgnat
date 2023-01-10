fcc62e48981919d9647a55aca9fdd10891c77a3d
Roman Sevryuk <Sevryuk.Roman@outlook.com>
2023-01-11 01:31:52 +0300

    refactor(Clock.tsx) refactoring of time and date display

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -44,7 +44,7 @@ function Clock() {
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
-                <span id={'hw9-day'}>{stringDay},</span>
+                <span id={'hw9-day'}>{stringDay}</span>,
                 <span id={'hw9-time'}>
                     <strong>{stringTime}</strong>
                 </span>
@@ -54,7 +54,7 @@ function Clock() {
                 <div className={s.more}>
                     {show ? (
                         <>
-                            <span id={'hw9-date'}>{stringDate},</span>
+                            <span id={'hw9-date'}>{stringDate}</span>,
                             <span id={'hw9-month'}>{stringMonth}</span>
                         </>
                     ) : (
