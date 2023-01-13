e07a711c627dce064613d1cdb587738272074294
Roman Sevryuk <Sevryuk.Roman@outlook.com>
2023-01-13 12:02:48 +0300

    commit for deploy homework hw11

--- src/s2-homeworks/hw05/HW5.tsx
+++ src/s2-homeworks/hw05/HW5.tsx
@@ -24,6 +24,7 @@ function HW5() {
                 <Pages/>
             </Layout>
         </HashRouter>
+
     )
 }
 
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import {SliderProps} from '@mui/material'
+import Slider from '@mui/material/Slider';
 
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
+
             sx={{ // стили для слайдера // пишет студент
-                
+                width: '300px',
+                heightMax: '3px',
+                color: '#00CC22',
+                '& .MuiSlider-rail': {
+                    backgroundColor: '#8B8B8B',
+                    opacity: '0.5',
+                },
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
