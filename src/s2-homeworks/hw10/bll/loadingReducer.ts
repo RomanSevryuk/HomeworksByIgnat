c61b9f0932f2fc45bc8e10d139dc239acdfd0e1e
Roman Sevryuk <Sevryuk.Roman@outlook.com>
2023-01-13 15:19:19 +0300

    commit for deploy homework hw12

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,6 +2,7 @@ const initState = {
     isLoading: false,
 }
 
+
 export const loadingReducer = (state = initState, action: LoadingActionType): typeof initState => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
